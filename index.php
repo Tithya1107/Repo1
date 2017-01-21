efc7ea44ee7e3bb4ae0b8ea9627cbb4ed6597a99
Tithya1107 <james54438@live.com>
2017-01-20 19:05:23 -0800

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+ Volume in drive C has no label.
+ Volume Serial Number is AC2A-8E12
+
+ Directory of C:\Users\James Khiev\Desktop\Git
+
+01/20/2017  07:04 PM    <DIR>          .
+01/20/2017  07:04 PM    <DIR>          ..
+01/20/2017  06:58 PM               356 index.css
+01/20/2017  07:04 PM                 0 index.php
+               2 File(s)            356 bytes
+               2 Dir(s)  66,222,493,696 bytes free
